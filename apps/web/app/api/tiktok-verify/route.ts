c815409a6a80ee1559636cf34748e1a0568823f9
codetix2020-hash <codetix2020@gmail.com>
2026-02-01 22:00:31 +0100

    fix: update tiktok verification token

--- apps/web/app/api/tiktok-verify/route.ts
+++ apps/web/app/api/tiktok-verify/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   return new NextResponse(
-    "tiktok-developers-site-verification=73USIYvtfVo4tubm56MdAN8dDYcPX2N1",
+    "tiktok-developers-site-verification=UIUhZL5eR1UnVYPkvQjP2g03yQbA9L2j",
     {
       headers: {
         "Content-Type": "text/plain",
